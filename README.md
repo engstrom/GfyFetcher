91ba2ee5a4ec6dcf8797d4cc09f053fe599a90c8
engstrom <dengstr@wgu.edu>
2013-12-29 13:10:31 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 GfyFetcher
 ==========
+
+A simple chrome extension that adds a context menu item for opening gifs via [http://gfycat.com/](Gfycat.com).
---
+++ src/content_script.js
@@ -0,0 +1,9 @@
+﻿function fetchGfy(info, tab) {	
+	var text = info.selectionText;
+	var url = 'http://gfycat.com/fetch/' + text;
+	chrome.tabs.create({
+		'url':url,
+	});
+}
+
+chrome.contextMenus.create({"title": "Fetch Gfycat of Link Address", "contexts":["selection"], "onclick": fetchGfy});
