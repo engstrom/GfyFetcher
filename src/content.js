f9ab2f9e518a6d4fabe4f349333ac1a3399830ee
engstrom <dengstr@wgu.edu>
2014-01-04 11:18:39 -0700

    can now right click on images in addition to links

---
+++ src/content.js
@@ -0,0 +1,12 @@
+﻿function fetchGfy(info) { 
+  var url = 'http://gfycat.com/fetch/' + (getUrl(info));
+    chrome.tabs.create({
+      'url':url
+  });
+}
+
+function getUrl(info) {
+  return info['srcUrl'] || info['linkUrl'];
+}
+
+chrome.contextMenus.create({"title": "Fetch Gfycat of Link Address", "contexts":["image", "link"], "onclick": fetchGfy});
