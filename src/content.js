1427f4b5c752210e2030f5bbed79603fe2d99d62
engstrom <dengstr@wgu.edu>
2014-03-13 20:27:35 -0600

    formatting

--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
-﻿function fetchGfy(info) { 
+﻿function fetchGfy(info) {
   var url = 'http://gfycat.com/fetch/' + (getUrl(info));
     chrome.tabs.create({
       'url':url
@@ -9,4 +9,6 @@ function getUrl(info) {
   return info['srcUrl'] || info['linkUrl'];
 }
 
-chrome.contextMenus.create({"title": "Fetch Gfycat of Image", "contexts":["image", "link"], "onclick": fetchGfy});
+chrome.contextMenus.create({"title": "Fetch Gfycat of Image",
+  "contexts":["image", "link"],
+  "onclick": fetchGfy});
